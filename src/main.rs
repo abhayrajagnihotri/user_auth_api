79db217beef7080fd5e954eb0431e9a0154520db
Abhay Raj Agnihotri <abhayrajsince2003@gmail.com>
2025-07-27 09:57:59 +0530

    Initial commit with existing files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+
+use axum:: {
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+asyn fn hello() -> &'static str{
+    "Hello from axum"
+}
+
+#[tokio::main]
+asyn fn main(){
+    let app = Router::new().route("/",get(hello));
+    let addr = SocketAddr::from(([127,0,0,1],3000));
+
+    println!("server runnning at http://{}", addr);
+
+    axum::Server::bind(&addr)
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
